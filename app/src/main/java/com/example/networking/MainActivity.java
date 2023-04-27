253cabe4099c5a749570cae119cb179344e94558
a22karja <a22karja@student-his.se>
2023-04-27 10:33:25 +0200

    Fixed the recycler with adapter and items

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
+    public ArrayList<item> items = new ArrayList<>(Arrays.asList(
+            new item("Matterhorn"),
+            new item("Mont Blanc"),
+            new item("Denali")
+    ));
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,16 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         setContentView(R.layout.activity_main);
 
         new JsonFile(this, this).execute(JSON_FILE);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(item item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
     }
 
     @Override
---
+++ app/src/main/java/com/example/networking/item.java
@@ -0,0 +1,14 @@
+package com.example.networking;
+
+@SuppressWarnings("WeakerAccess")
+public class item {
+    private String title;
+
+    public  item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
